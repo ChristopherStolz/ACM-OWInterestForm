b6c0dddc4de99949ec5247cf88a96a0c9e33b37e
ChristopherStolz <ChristoherStolz@github.com>
2017-11-06 12:47:55 -0500

    Initial commit

---
+++ src/main/java/edu/nyit/acmInterest/AcmInterestApplication.java
@@ -0,0 +1,12 @@
+package edu.nyit.acmInterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmInterestApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/nyit/acmInterest/AcmInterestApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.nyit.acmInterest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcmInterestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
